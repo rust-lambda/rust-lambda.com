9e70cbf7e3b22be5a422cbda5658b2e55dcf6b0f
Luciano Mammino <lucianomammino@gmail.com>
2024-04-22 21:18:36 +0100

    Updated deploy workflow

--- src/consts.ts
+++ src/consts.ts
@@ -1,1 +1,3 @@
 export const SITE_DOMAIN = 'rust-lambda.com'
+export const BUY_LINK = 'https://loige.gumroad.com/l/rust-lambda/early-access'
+export const CHECKOUT_LINK = 'https://app.gumroad.com/checkout?product=mheuj&quantity=1&code=early-access&referrer=http%3A%2F%2Flocalhost%3A4321%2F'
