d7e9f71a065364525ae7cef342f377672627cbfc
Luciano Mammino <lucianomammino@gmail.com>
2024-04-22 21:39:34 +0100

    fixing config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
 	darkMode: ["class"],
 	content: [
 		'./src/**/*.{ts,tsx,astro,js,jsx,mdx}'
@@ -72,3 +72,5 @@ module.exports = {
 	},
 	plugins: [require("tailwindcss-animate")],
 }
+
+export default config
