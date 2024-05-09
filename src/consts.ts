b6361f1d45ec12436ae0a8c75c49883ff033ce86
Alessandro Miliucci <lifeisfoo@gmail.com>
2024-05-09 09:58:03 +0200

    Update consts.ts (#1)

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@ export const SITE_AUTHORS = ["Luciano Mammino", "James Eastham"]
 export const SITE_TITLE = `Crafting Lambda Functions in Rust - eBook by ${SITE_AUTHORS.join(' and ')}`
 export const SITE_DESCRIPTION = 'A practical guide on how to build efficient, sustainable, cost-effective serverless AWS solutions with the Rust programming language.'
 export const BUY_LINK = 'https://loige.gumroad.com/l/rust-lambda/early-access'
-export const CHECKOUT_LINK = 'https://app.gumroad.com/checkout?product=mheuj&quantity=1&code=early-access&referrer=http%3A%2F%2Flocalhost%3A4321%2F'
+export const CHECKOUT_LINK = 'https://app.gumroad.com/checkout?product=mheuj&quantity=1&code=early-access'
 export const PRICE_FULL = '45.90'
 export const PRICE_DISCOUNTED = '18.36'
 export const NAV_LINKS = [
